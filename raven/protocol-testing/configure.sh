d4f2bdab30fafb8a3a7536da01ec57e1ab996c18
Lincoln Thurlow <lincoln@isi.edu>
2024-07-26 16:07:17 -0700

    change topology for more flexibility
    Signed-off-by: Lincoln Thurlow <lincoln@isi.edu>

--- raven/protocol-testing/configure.sh
+++ raven/protocol-testing/configure.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
+set -e
+
 if [[ $EUID -ne 0 ]]; then
     echo "requires root access to run ansible playbook"
     exit 1
 fi
 
-#ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i .rvn/ansible-hosts -e 'ansible_python_interpreter=/usr/bin/python3'  ./ansible/plays/update_all_hosts.yml
-#ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i .rvn/ansible-hosts -e 'ansible_python_interpreter=/usr/bin/python3'  ./ansible/plays/update_all_switches.yml
-#ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --limit "!s0" -i .rvn/ansible-hosts -e 'ansible_python_interpreter=/usr/bin/python3' -e "@./ansible/variables/config.yml" ./ansible/plays/configure_ip4_network.yml
-#ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_ROLES_PATH=ansible/roles \
-#    ansible-playbook --limit "!s0" -i .rvn/ansible-hosts \
-#    -e 'ansible_python_interpreter=/usr/bin/python3' \
-#    -e "@./ansible/variables/config.yml" \
-#    ./ansible/plays/install_nebula.yml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i .rvn/ansible-hosts -e 'ansible_python_interpreter=/usr/bin/python3'  ./ansible/plays/update_all_hosts.yml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i .rvn/ansible-hosts -e 'ansible_python_interpreter=/usr/bin/python3'  ./ansible/plays/update_all_switches.yml
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --limit "!s0" -i .rvn/ansible-hosts -e 'ansible_python_interpreter=/usr/bin/python3' -e "@./ansible/variables/config.yml" ./ansible/plays/configure_ip4_network.yml
+ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_ROLES_PATH=ansible/roles \
+    ansible-playbook --limit "!s0" -i .rvn/ansible-hosts \
+    -e 'ansible_python_interpreter=/usr/bin/python3' \
+    -e "@./ansible/variables/config.yml" \
+    ./ansible/plays/install_nebula.yml
 
-#ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_ROLES_PATH=ansible/roles \
-#    ansible-playbook --limit "!s0" -i .rvn/ansible-hosts \
-#    -e 'ansible_python_interpreter=/usr/bin/python3' \
-#    -e "@./ansible/variables/config.yml" \
-#    ./ansible/plays/install_etcd.yml
+ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_ROLES_PATH=ansible/roles \
+    ansible-playbook --limit "!s0" -i .rvn/ansible-hosts \
+    -e 'ansible_python_interpreter=/usr/bin/python3' \
+    -e "@./ansible/variables/config.yml" \
+    ./ansible/plays/install_etcd.yml
  
-#ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_ROLES_PATH=ansible/roles \
-#    ansible-playbook --limit "!s0" -i .rvn/ansible-hosts \
-#    -e 'ansible_python_interpreter=/usr/bin/python3' \
-#    -e "@./ansible/variables/config.yml" \
-#    ./ansible/plays/install_avoid_go.yml
-#
+ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_ROLES_PATH=ansible/roles \
+    ansible-playbook --limit "!s0" -i .rvn/ansible-hosts \
+    -e 'ansible_python_interpreter=/usr/bin/python3' \
+    -e "@./ansible/variables/config.yml" \
+    ./ansible/plays/install_avoid_go.yml
+
 ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_ROLES_PATH=ansible/roles \
     ansible-playbook --limit "!s0" -i .rvn/ansible-hosts \
     -e 'ansible_python_interpreter=/usr/bin/python3' \
